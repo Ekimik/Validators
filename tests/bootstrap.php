24aa4cf98ec31a72e2122b2c7a0eeed2c2bcbf04
Jan Jíša <j.jisa@seznam.cz>
2017-12-25 16:59:57 +0100

    cleaning code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,11 @@
 <?php
 
-/**
- * Tests bootstrap file
- * @author Jan Jíša <j.jisa@seznam.cz>
- */
-
 require __DIR__ . '/../vendor/autoload.php';
 
 // app base directory
 define('TESTS_DIR',  __DIR__);
 
 $loader = new Nette\Loaders\RobotLoader;
-$loader->addDirectory(__DIR__ . '/../src');
 $loader->addDirectory(TESTS_DIR . '/src/Mocks');
 $loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(__DIR__ . '/tmp'));
 $loader->register();
